3ca5146fd7260c979640190422612deab6f5c0da
dliroulet <didier.liroulet@ocsinventory-ng.org>
2011-02-21 08:37:47 +0100

    Adding CCapKeyFinder class

---
+++ Agent/CapKeyFinder.h
@@ -0,0 +1,20 @@
+//====================================================================================
+// Open Computer and Software Inventory Next Generation
+// Copyright (C) 2010 OCS Inventory NG Team. All rights reserved.
+// Web: http://www.ocsinventory-ng.org
+
+// This code is open source and may be copied and modified as long as the source
+// code is always made freely available.
+// Please refer to the General Public Licence V2 http://www.gnu.org/ or Licence.txt
+//====================================================================================
+
+#pragma once
+
+#include "OCSInventory.h"
+
+class CCapKeyFinder : public CCapacityAbstract
+{
+public:
+	CCapKeyFinder(void);
+	~CCapKeyFinder(void);
+};
