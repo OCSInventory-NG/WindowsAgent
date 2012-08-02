8cad7984588a77493d2995ffda2e3f2abe2eff46
dliroulet <didier.liroulet@ocsinventory-ng.org>
2012-08-02 20:17:35 +0200

    Update version to 2.0.4.5

---
+++ Download/resource.h
@@ -0,0 +1,17 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Download.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDR_MAINFRAME                   128
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           102
+#endif
+#endif
