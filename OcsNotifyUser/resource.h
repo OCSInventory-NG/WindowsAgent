7814446835965d44b842eab45910281b0548db7f
dliroulet <didier.liroulet@ocsinventory-ng.org>
2013-03-06 17:08:57 +0100

    Add missing OcsNotifyUser/resource.h to repository

---
+++ OcsNotifyUser/resource.h
@@ -0,0 +1,30 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by OcsNotifyUser.rc
+//
+#define IDD_DOWNLOAD_DIALOG             102
+#define IDD_WAIT_SETUP_DIALOG           103
+#define IDD_CU                          104
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT1                       1001
+#define IDC_DELAY                       1002
+#define IDC_STATUS                      1003
+#define IDC_VAL                         1004
+#define IDC_LABEL_TEXT                  1005
+#define IDS_AUTO_VALIDATE_STRING        1006
+#define IDS_WAIT_SETUP_STRING           1007
+#define IDS_CANCEL_STRING               1008
+#define IDS_DELAY_STRING                1009
+#define IDS_AUTO_REBOOT_STRING          1010
+#define IDS_REBOOT_REQUIRED_STRING      1011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           106
+#endif
+#endif
