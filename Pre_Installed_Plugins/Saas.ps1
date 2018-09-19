fb85b2ef8226b51628731808650038ce222eb1c3
Charlene Auger <charlene.auger50@gmail.com>
2018-09-19 09:26:54 +0200

    Update saas.ps1

--- Pre_Installed_Plugins/Saas.ps1
+++ Pre_Installed_Plugins/Saas.ps1
@@ -1,8 +1,8 @@
-Function Get-DNSClientCache{  
+Function Get-DNSClientCache($pwlang){  
     $hash = @{}  
 
     Invoke-Expression "ipconfig /displaydns" |  
-    Select-String -Pattern "Nom d'enregistrement" -Context 0,5 |  
+    Select-String -Pattern $pwlang -Context 0,5 |  
         %{  
 			$xml += "<SAAS>`n"
 			$xml += "<ENTRY>" + ($_.Line -Split ":")[1].Trim().ToLower()  + "</ENTRY>`n"
@@ -14,6 +14,15 @@ Function Get-DNSClientCache{
 	return $xml
 	
 } 
+
+$lang = Get-Culture
+
+if($lang.Name -eq "fr-FR"){
+	$pwlang = "Nom d'enregistrement"
+}else{
+	$pwlang = "Record Name"
+}
+
  
-$dns = Get-DnsClientCache  
+$dns = Get-DnsClientCache($pwlang) 
 echo $dns
