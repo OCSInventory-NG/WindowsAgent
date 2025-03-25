f62223504f4f95c64e268033e169f5d1bddcd665
charleneauger <charlene.auger@factorfx.com>
2025-03-25 14:51:29 +0100

    fix(saas): fix saas plugin if empty data

--- Pre_Installed_Plugins/Saas.ps1
+++ Pre_Installed_Plugins/Saas.ps1
@@ -3,11 +3,26 @@ Function Get-DNSClientCache($pwlang){
 
     Invoke-Expression "ipconfig /displaydns" |  
     Select-String -Pattern $pwlang -Context 0,5 |  
-        %{  
+        %{
 			$xml += "<SAAS>`n"
-			$xml += "<ENTRY>" + ($_.Line -Split ":")[1].Trim().ToLower()  + "</ENTRY>`n"
-			$xml += "<DATA>" + ($_.Context.PostContext[4] -Split ":")[1].Trim().ToLower() + "</DATA>`n"
-			$xml += "<TTL>" + ($_.Context.PostContext[1] -Split ":")[1].Trim() + "</TTL>`n"
+			if ($_.Line.Contains(":")) {
+				$xml += "<ENTRY>" + ($_.Line -Split ":")[1].Trim().ToLower()  + "</ENTRY>`n"
+			} else {
+				$xml += "<ENTRY />`n"
+			}
+
+			if ($_.Context.PostContext[4].Contains(":")) {
+				$xml += "<DATA>" + ($_.Context.PostContext[4] -Split ":")[1].Trim().ToLower() + "</DATA>`n"
+			} else {
+				$xml += "<DATA />`n"
+			}
+
+			if ($_.Context.PostContext[1].Contains(":")) {
+				$xml += "<TTL>" + ($_.Context.PostContext[1] -Split ":")[1].Trim() + "</TTL>`n"
+			} else {
+				$xml += "<TTL />`n"
+			}
+
 			$xml += "</SAAS>`n"           
         }  
 	
@@ -27,6 +42,5 @@ if($lang.Name -eq "fr-FR"){
 	$pwlang = "Record Name"
 }
 
- 
-$dns = Get-DnsClientCache($pwlang) 
+$dns = Get-DnsClientCache($pwlang)
 echo $dns
