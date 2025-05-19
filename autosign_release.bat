031c2bbead282017446f75a128084b45fcf74b4e
charleneauger <charlene.auger@factorfx.com>
2025-05-19 10:05:03 +0200

    refactor(script): update auto sign script

--- autosign_release.bat
+++ autosign_release.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal
-signtool sign /fd SHA256 /n "Factor FX" /t http://timestamp.digicert.com Release\*.exe
-signtool sign /fd SHA256 /n "Factor FX" /t http://timestamp.digicert.com Release\*.dll
+signtool sign /fd SHA256 /n "FactorFX" /t http://timestamp.digicert.com Release\*.exe
+signtool sign /fd SHA256 /n "FactorFX" /t http://timestamp.digicert.com Release\*.dll
 endlocal
